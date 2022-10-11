2acff892d129ba45c4ca928630627a8768e1d16d
Erick Santos <erickosantos.dev@gmail.com>
2022-10-11 20:45:52 -0300

    test: 💍 Adding text component

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,50 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './Text'
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  args: {
+    children: 'Lorem ipsum.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      control: {
+        type: 'inline-radio',
+        options: ['sm', 'md', 'lg'],
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm',
+  }
+}
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg',
+  }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Testando</p>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
